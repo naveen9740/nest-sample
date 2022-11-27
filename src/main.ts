608eb9cf2f30323672d58e3ff2925605d2d13ece
naveen9740 <naveen.kamath002@gmail.com>
2022-11-27 15:27:44 +0530

    morgan

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+import * as fs from 'fs';
+var morgan = require('morgan');
+var path = require('path');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
   const config = new DocumentBuilder()
@@ -12,6 +16,11 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
+
+  const accessLogStream = fs.createWriteStream(path.join('./', 'logs.log'), {
+    flags: 'a',
+  });
+  app.use(morgan('combined', { stream: accessLogStream }));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
